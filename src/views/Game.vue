9338dcd0461b08d81c3fefa41337dddd958abd10
AymericPost <aymeric.post@gmail.com>
2019-07-27 13:46:59 +0200

    :sparkles: Game status bar

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,18 +1,22 @@
 <template>
     <section>
+        <game-stats />
         <grid :xMax="xMax" :yMax="yMax" :key="gameTick"/>
     </section>
 </template>
 
 <script>
 import grid from "../components/Grid";
+import GameStats from "../components/GameStats"
 import {mapState} from "vuex";
 import {mapActions} from "vuex";
 
 export default {
     name: "game",
     components: {
-        grid
+        grid,
+        "game-stats": GameStats
+
     },
     computed: {
         ...mapState(["xMax", "yMax", "gameTick"])
